e3a78a129b6a723cd9d0f1160bedc53fa38fcfe0
Vanessa Monteiro <121586432+vanessascmonteiro@users.noreply.github.com>
2023-02-12 22:53:38 -0300

    Add files via upload

--- Semana 1/Program.cs
+++ Semana 1/Program.cs
@@ -1,13 +1,49 @@
-﻿int tabuada; 
-int num = 0;
-int resultado;
+﻿float nota1, nota2, nota3, media; 
+string aluno;
 
-Console.WriteLine("Informe o número da tabuada: "); 
-tabuada = int.Parse(Console.ReadLine());
 
-for(num = 0; num <= 10; num++){
-    resultado = (tabuada * num);
-    Console.WriteLine($"O resultado da tabuada é  " + resultado);
+Console.WriteLine("Digite o nome do aluno");
+aluno = (Console.ReadLine());
+
+Console.WriteLine("Digite a nota1");
+nota1 = float.Parse(Console.ReadLine());
+
+Console.WriteLine("Digite a nota2");
+nota2 = float.Parse(Console.ReadLine());
+
+Console.WriteLine("Digite a nota3");
+nota3 = float.Parse(Console.ReadLine());
+
+media = (nota1 + nota2 + nota3) /3;
+
+float[] nota = new float[3];
+nota[0] = nota1;
+nota[1] = nota2;
+nota[2] = nota3;
+
+if(media>=6){
+    Console.WriteLine(aluno);
+    Console.WriteLine("N1 " + nota1);
+    Console.WriteLine("N2 " + nota2);
+    Console.WriteLine("N3 " + nota3);
+    Console.WriteLine("media " + media + " aprovado");
+
+}
+else if(media>=5){
+    Console.WriteLine(aluno);
+    Console.WriteLine("N1 " + nota1);
+    Console.WriteLine("N2 " + nota2);
+    Console.WriteLine("N3 " + nota3);
+    Console.WriteLine("media " + media + " em recuperação");
+
+}
+else {
+    Console.WriteLine(aluno);
+    Console.WriteLine("N1 " + nota1);
+    Console.WriteLine("N2 " + nota2);
+    Console.WriteLine("N3 " + nota3);
+    Console.WriteLine("media " + media + " reprovado");
+    
 
 }
 
